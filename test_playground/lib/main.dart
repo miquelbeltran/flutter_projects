8353f1058a141602d076a852a1d27134d3158c1e
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2018-03-15 11:57:05 +0100

    pass the GitHubServiceContext on the App building

--- test_playground/lib/main.dart
+++ test_playground/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:test_playground/github_service.dart';
+import 'package:test_playground/github_service_context.dart';
 import 'package:test_playground/network_text.dart';
 
 void main() => runApp(new MyApp());
@@ -7,20 +9,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    return new GitHubServiceContext(
+      gitHubService: new GitHubService(),
+      child: new MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+          // counter didn't reset back to zero; the application is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: new NetworkText(),
       ),
-      home: new NetworkText(),
     );
   }
 }
